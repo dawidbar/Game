5f56f46ef41d7c0e7f611d30e49bcbe45eda6370
Dawid Barsznica <dbarsznica@gmail.com>
2019-02-19 12:22:43 +0100

    Add modal

--- js/index.js
+++ js/index.js
@@ -6,21 +6,25 @@ var paper = document.getElementById("button2");
 var scissors = document.getElementById("button3");
 var cScorepoints = document.getElementById("cscore");
 var pScorepoints = document.getElementById("pscore");
-var cpoint = 0;
-var ppoint = 0;
+var tablemaker = document.querySelector('.table');
+var params = {
+    cpoint: 0,
+    ppoint: 0,
+    rounds: 0,
+    roundsCounter: 0,
+    progress: []
+};
 var rounds;
 var howmanyrounds = document.getElementById("rounds1");
+var gamebtn = document.querySelectorAll(".player-move");
 
 
-paper.addEventListener("click", function () {
-    playerMove("PAPER");
-});
-rock.addEventListener("click", function () {
-    playerMove("ROCK");
-});
-scissors.addEventListener("click", function () {
-    playerMove("SCISSORS");
-});
+for (var i = 0; i < gamebtn.length; i++) {
+    gamebtn[i].addEventListener("click", function () {
+        var btnchoice = this.getAttribute("data-move");
+        playerMove(btnchoice);
+    });
+};
 
 function playerMove(playerPick) {
     result(playerPick, computerMove());
@@ -40,38 +44,53 @@ function computerMove() {
 function result(playerMove, computerMove) {
     if (playerMove === computerMove) {
         output.innerHTML = 'Remis! Wybrałeś: ' + playerMove + ' komputer wybrał: ' + computerMove;
+        params.roundsCounter++;
+        params.winner = "Remis";
     } else if (
         (playerMove === "ROCK" && computerMove === "PAPER") ||
         (playerMove === "SCISSORS" && computerMove === "ROCK") ||
         (playerMove === "PAPER" && computerMove === "SCISSORS")
     ) {
         output.innerHTML = 'Komputer zdobywa punkt! Wybrałeś: ' + playerMove + ' komputer wybrał: ' + computerMove;
-        cpoint++;
+        params.cpoint++;
+        params.roundsCounter++;
+        params.lost++;
+        params.winner = "Komputer";
     } else {
         output.innerHTML = 'Zdobywasz punkt! Wybrałeś: ' + playerMove + ' komputer wybrał: ' + computerMove;
-        ppoint++;
+        params.ppoint++;
+        params.roundsCounter++;
+        params.wins++;
+        params.winner = "Gracz";
     }
+    params.progress.push({
+        gameRounds: params.roundsCounter,
+        gamePlayerMove: playerMove,
+        gameComputerMove: computerMove,
+        roundWinner: params.winner,
+        finalResult: params.wins + ' - ' + params.lost
+    })
     points();
     gameover();
 };
 
 function points() {
-    cScorepoints.innerHTML = cpoint;
-    pScorepoints.innerHTML = ppoint;
+    cScorepoints.innerHTML = params.cpoint;
+    pScorepoints.innerHTML = params.ppoint;
 };
 
 newgame.addEventListener("click", gamestart);
 
 function gamestart() {
-    rounds = prompt("Ile rund?");
-    if (isNaN(rounds)) {
+    params.rounds = prompt("Ile rund?");
+    if (isNaN(params.rounds)) {
         output.innerHTML = 'Wpisz liczbę rund';
-    } else if (rounds <= 0) {
+    } else if (params.rounds <= 0) {
         output.innerHTML = 'Liczba rund musi być więkasza od zera';
     } else {
         output.innerHTML = 'Powodzenia!';
-        ppoint = 0;
-        cpoint = 0;
+        params.ppoint = 0;
+        params.cpoint = 0;
         cScorepoints.innerHTML = 0;
         pScorepoints.innerHTML = 0;
         rock.disabled = false;
@@ -82,28 +101,73 @@ function gamestart() {
         scissors.classList.toggle('btn-disabled');
     }
     round();
+    refresh()
 };
 
 function round() {
-    howmanyrounds.innerHTML = rounds;
+    howmanyrounds.innerHTML = params.rounds;
 };
 
 function gameover() {
-    if (ppoint == rounds) {
-        output.innerHTML = 'Wygrałeś! Zdobyłeś: ' + ppoint + ' ptk';
+    var header = document.querySelector('.header');
+    if (params.ppoint == params.rounds) {
+        header.innerHTML = 'Wygrałeś! Zdobyłeś: ' + params.ppoint + ' ptk';
         rock.disabled = true;
         rock.classList.toggle('btn-disabled');
         paper.disabled = true;
         paper.classList.toggle('btn-disabled');
         scissors.disabled = true;
         scissors.classList.toggle('btn-disabled');
-    } else if (cpoint == rounds) {
-        output.innerHTML = 'Niestety tym razem się nie udało! Wygrywa Komputer!';
+        showModal()
+    } else if (params.cpoint == params.rounds) {
+        header.innerHTML = 'Niestety tym razem się nie udało! Wygrywa Komputer!';
         rock.disabled = true;
         rock.classList.toggle('btn-disabled');
         paper.disabled = true;
         paper.classList.toggle('btn-disabled');
         scissors.disabled = true;
         scissors.classList.toggle('btn-disabled');
+        showModal()
     }
 };
+
+//modal
+function showModal(){
+    event.preventDefault();
+    document.querySelector('#modal-overlay').classList.add('show');
+    var modal = document.querySelector('.modal');
+    modal.classList.add('show');
+    buildTable()
+}
+
+var hideModal = function (event) {
+    event.preventDefault();
+    document.querySelector('#modal-overlay').classList.remove('show');
+};
+
+var closeButtons = document.querySelectorAll('.modal .close');
+for (var i = 0; i < closeButtons.length; i++) {
+    closeButtons[i].addEventListener('click', hideModal);
+}
+
+document.querySelector('#modal-overlay').addEventListener('click', hideModal);
+
+var modals = document.querySelectorAll('.modal');
+
+for (var i = 0; i < modals.length; i++) {
+    modals[i].addEventListener('click', function (event) {
+        event.stopPropagation();
+    });
+}
+
+//table
+function refresh() {
+    params.wins = params.lost = 0;
+    params.roundsCounter = 0;
+    params.winner = 0;
+    params.progress = [];
+  }
+
+function buildTable() {
+    
+}
